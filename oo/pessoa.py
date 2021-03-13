e24cdaa5eb74ec98b31e16df090cd20be0138ed5
Celso <celsoclaudio206@gmail.com>
2021-03-12 21:45:41 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=42):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return 'Óla'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Celso')
+    # p.nome = 'Celso'
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
